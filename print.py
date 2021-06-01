c969b0c717a58d99f56dc80b998d1aa892b9a69f
Spacestronaut <Abelr6099@gmail.com>
2021-06-01 20:02:29 +0700

    First Python Commit

---
+++ print.py
@@ -0,0 +1,4 @@
+# dalam python,kamu tidak perlu mendeklarasikan sebuah class ataupun fungsi dulu untuk mencetak apapun lewat console.
+# cukup dengan print saja kamu sudah bisa mencetak sesuatu ke console mu
+
+print("Hello World")
---
+++ variable.py
@@ -0,0 +1,11 @@
+# sama halnya dengan bahasa pemrograman lainnya,python pun memiliki sebuah variable. istilahnya variable ini untuk memberikan rumah terhadap elemen atau code yang akan kita gunakan nantinya
+# cara membuat variable di python cukup mudah,tanpa perlu kata kunci int,string atau var.
+
+# contoh :
+
+x = 5
+str = "Hello World"
+bl = True
+
+# variable dapat berupa string,integer maupaun boolean
+# cukup mudah bukan?
